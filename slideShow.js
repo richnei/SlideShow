2caf3b24ba1c57047dddb65630c58aa97df9ad6a
Richnei <rich.nei@hotmail.com>
2022-01-16 18:48:23 -0300

    cs

--- slideShow.js
+++ slideShow.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const images = [
-  { id: "1", url: "./img/1_morro-da-cruz.jpg" },
-  { id: "2", url: "./img/2_ponte-luz.jpg" },
+  { id: "1", url: "./img/1_ponte-luz.jpg" },
+  { id: "2", url: "./img/2_morro-da-cruz.jpg" },
   { id: "3", url: "./img/3_mar.jpg" },
   { id: "4", url: "./img/4_lagoa-conceicao.jpg" },
   { id: "5", url: "./img/5_trilha.jpg" },
@@ -14,7 +14,7 @@ const container = document.querySelector("#container-items");
 const loadImages = (images, container) => {
   images.forEach((image) => {
     container.innerHTML += `
-        <div class = 'slide'>
+        <div class = 'item'>
             <img src = '${image.url}'>
         </div>
     `;
